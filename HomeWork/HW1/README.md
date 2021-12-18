8617293553085dd42add72eb3e459eb35390f521
YeoJiSu <duwltn1301@naver.com>
2021-12-18 21:14:12 +0900

    웹응용 프로그래밍 과제

---
+++ HomeWork/HW1/README.md
@@ -0,0 +1,5 @@
+## CSS3 관련 숙제
+
+- CSS pagination을 이용하여 select 구현
+
+- flex-box와 object-fit를 이용하여 그림 하나를 16개로 나누어 분리함, 그림 사이에는 box가 쳐지게 함
---
+++ HomeWork/HW6/index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+    <meta charset="utf-8" />
+</head>
+
+<body>
+    <form action="submit.php" method="post">
+        학번: <input type="text" name="studentID" />
+        이름: <input type="text" name="name" />
+        휴대폰번호: <input type="text" name="phonenum" />
+        <input type="submit" value="입력" />
+    </form>
+
+    <!-- <form action="delete.php" method="post">
+        삭제할 이름은? <input type="text" name="name" />
+        <input type="submit" value="삭제" />
+    </form> -->
+</body>
+
+</html>
